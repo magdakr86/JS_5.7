5bce2c20336cc3ee09f9303daa4a6d2348bf9103
Magdalena Michalska <magda@MacBook-Pro-Magdalena.local>
2017-12-17 09:25:23 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '15129965769-ctd5r5d7h5mvfmsralkridje42dqbp6f.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '-uxigys-D0XdR3zC285_YKM6',
+    'CALLBACK_URL': 'http://localhost:8080/auth/google/callback'
+};
